f80c5302fee7e5b9a764d286add3bba7885f347e
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-14 11:05:34 +0900

    feat: hero components

---
+++ src/components/hero.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "../styles/hero.css"
+
+export const Hero = () => {
+    return (
+        <div className="hero">
+            <h1 className="hero-text">
+                Welcome to my blog!<br />
+                Enjoy lots of posts.<br />
+            </h1>
+            <div className="hero-author">
+                <img src="https://avatars.githubusercontent.com/u/45593212" className="hero-author-image" alt="avatar" />
+                <p className="hero-author-text">
+                    Written by XXXX.<br />
+                    Front Engineer at Hoge.Inc. Love JavaScript.
+                </p>
+            </div>
+        </div>
+    )
+}
