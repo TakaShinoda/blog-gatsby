3195a1148ec2af385bcd5b44d9b5b7904f68e1cb
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-14 10:56:50 +0900

    レイアウト作成

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/layout.css"
+import { Header } from "./header"
+
+export const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Header />
+            <main>{children}</main>
+        </div>
+    )
+}
