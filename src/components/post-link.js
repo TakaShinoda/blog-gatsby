400eef1bb1e828f74631133a9a8def6169c593df
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-17 00:33:54 +0900

    feat: 記事を動的にルーティング

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,20 +1,27 @@
 import React from "react"
 import "../styles/post-link.css"
+import { Link } from "gatsby"
+
 
 export const PostLink = ({ post }) => {
     const { title, updatedAt, image } = post
     const description = post.description.description
+    const pageLink = `/post/${post.slug}/`
     console.log(image.file.url)
+    const imgUrl = "https:" + image.file.url
+
     return (
-        <div className="post-link">
-            <div>
-                <img src={image.file.url} className="post-link-image" alt="post-cover" />
+        <Link to={pageLink} className="post-link-anchor">
+            <div className="post-link">
+                <div>
+                    <img src={imgUrl} className="post-link-image" alt="post-cover" />
+                </div>
+                <div className="post-link-text">
+                    <h2>{title}</h2>
+                    <p className="post-link-body">{description}</p>
+                    <p className="post-link-date">{updatedAt}</p>
+                </div>
             </div>
-            <div className="post-link-text">
-                <h2>{title}</h2>
-                <p className="post-link-body">{description}</p>
-                <p className="post-link-date">{updatedAt}</p>
-            </div>
-        </div>
+        </Link>
     )
 }
