e836ad284b91bbbaa70dc93138817597256228f8
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-18 23:08:04 +0900

    feat: seoコンポーネントを作成

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,17 @@
 import React from "react"
 import "../styles/post.css"
 import { Layout } from "../components/layout"
+import { SEO } from "../components/seo"
 
 // pageContextには、gatsby-node.jsのcreatePageの引数のcontextで指定したデータが渡ってくる
 export default function Post({ pageContext }) {
     const { title, updatedAt, image } = pageContext.post
     const body = pageContext.post.body.childMarkdownRemark.html
+    const description = pageContext.post.description.description;
 
     return (
         <Layout>
+            <SEO title={title} description={description} />
             <div className="post-header">
                 <h1>{title}</h1>
                 <p className="post-date">{updatedAt}</p>
