98f4600e52445cff7a804826b6b235674ce627be
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-21 18:30:42 +0900

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,6 @@ import { Layout } from "../components/layout"
 import { SEO } from "../components/seo"
 import NOT_FOUND_IMG from "../../static/404.jpg"
 import "../styles/404.css"
-import { image } from "../../src/components/image"
 
 export default function NotFound() {
   return (
@@ -14,8 +13,7 @@ export default function NotFound() {
         <h1>404 Not Found</h1>
         <Link to="/">トップページへ</Link>
       </div>
-      {/* <img src={NOT_FOUND_IMG} className="not-found-image" alt="not found" /> */}
-      <Image assetUrl="404.jpg" alt="not found" className="not-found-image" />
+      <img src={NOT_FOUND_IMG} className="not-found-image" alt="not found" />
     </Layout>
   )
 }
