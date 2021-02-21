9035a171cecd47cb79a112f9b84588cecedae4e1
Takahiro <tkhr_621@docomo.ne.jp>
2021-02-21 12:42:11 +0900

    fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,6 @@ import "../styles/footer.css"
 
 export const Footer = () => {
   return (
-    <footer className="footer">©︎ {new Date().getFullYear()} Dev Blog</footer>
+    <footer className="footer">©︎ {new Date().getFullYear()} tttttt_621_s</footer>
   )
 }
